3feda8011be46abe78b06189b2178b10533c8971
Joe Bloggs <vapniks@yahoo.com>
2015-10-06 20:01:19 +0100

    Update documentation

--- extract-text.el
+++ extract-text.el
@@ -42,10 +42,34 @@
 ;; Bitcoin donations gratefully accepted: 1FgnGFwRES9MGzoieRDWLmLkjqMT3AsjQF
 ;;
 ;; This library provides functions for programmatically extracting text from buffers.
-;;
-;;
+;; The `extract-matching-strings' and `extract-matching-rectangles' allow you to perform
+;; simple extractions, but for more complex tasks there is the `extract-text' macro.
+;; This macro allows complex text extraction specifications using a kind of dsl for text extraction.
+;; You can use included wrapper functions for picking out individual bits of text, or define your own
+;; and save them in `extract-text-wrappers'. You can specify how many times to repeat each individual extraction,
+;; how to handle errors or missing values, restrict the buffer for finding certain extractions, how to structure
+;; &/or transform the output, etc. See the docstring for `extract-text' for more details.
+;; 
 ;;;;
 
+;;; Functions:
+;;
+;; `match-strings' : Return a list of all strings matched by last search.
+;; `match-strings-no-properties' : Return a list of all strings matched by last search.
+;; `extract-matching-strings' : Extract strings from current buffer that match subexpressions of REGEXP.
+;; `extract-matching-rectangle' : Extract a rectangle of text (list of strings) from the current buffer.
+;; `copy-rectangle-to-buffer' : Copy a rectangular region of the current buffer to a new buffer.
+;;
+;;; Macros:
+;;
+;; `extract-text' : Extract text from buffer according to specifications in ARGS.
+;; `extract-text-from-buffers' : Extract text from buffers listed in BUFFERS or matching regexp BUFFERS.
+;; `extract-text-from-files' : Extract text from FILES.
+;;
+;;; Options: 
+;;
+;; `extract-text-wrappers' : A list of wrapper functions that can be used with `extract-text'.
+;;
 
 ;;; Installation:
 ;;
